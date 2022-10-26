3188c1b929fe241e03d85f1929a9c22bac2867d0
Przemek Denkiewicz <przemyslaw.denkiewicz@starburstdata.com>
2022-10-26 15:44:48 +0200

    update

--- models/source/src_payments.sql
+++ models/source/src_payments.sql
@@ -12,7 +12,6 @@ renamed as (
         id as payment_id,
         order_id,
         payment_method,
-
         -- `amount` is currently stored in cents, so we convert it to dollars
         amount / 100 as amount
 
