f464208c71658919386e46ed1a6f6049573e04f1
Przemek Denkiewicz <przemyslaw.denkiewicz@starburstdata.com>
2022-11-03 17:45:02 +0100

    Update failing test

--- tests/assert_no_overlapping_sessions.sql
+++ tests/assert_no_overlapping_sessions.sql
@@ -1,5 +1,10 @@
 select
-    sc1.visitorid || '_' || cast(sc2.session_sequence as varchar) as session_id
+    sc1.session_started,
+    sc1.session_ended,
+    sc1.session_ended,
+    sc2.session_started,
+    sc1.visitorid,
+    sc2.visitorid
 from {{ ref('sessionized_clicks') }} sc1
 join {{ ref('sessionized_clicks') }} sc2
 on sc1.session_started > sc2.session_ended and sc1.session_ended < sc2.session_started and sc1.visitorid = sc2.visitorid
